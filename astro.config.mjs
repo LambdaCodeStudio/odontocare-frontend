dc7095db34d1ea6a285fc36f850bea64838bff9e
Alejo <alejocstaf@proton.me>
2025-06-09 18:34:53 -0300

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: "hybrid",
   vite: {
     ssr: {
       noExternal: ['flatpickr'] 
